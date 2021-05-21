c3492903462b0c1f9004a3d96e386a887885a439
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2021-05-22 00:24:23 +1200

    Draft 1, as written

--- Loki's Bizarre Adventure Unity/Assets/LokiControls.cs
+++ Loki's Bizarre Adventure Unity/Assets/LokiControls.cs
@@ -7,20 +7,34 @@ public class LokiControls : MonoBehaviour
 {
     PlayerControls controls;
 
+    Vector2 move;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void Awake()
+    void Awake()
     {
         controls = new PlayerControls();
+        controls.Loki.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.Loki.Move.canceled += ctx => move = Vector2.zero;
+    }
+
+    void Update()
+    {
+        Vector2 m = new Vector2(move.x, 0f) * Time.deltaTime;
+        transform.Translate(m, Space.World);
+    }
+
+    void OnEnable()
+    {
+        controls.Loki.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Loki.Disable();
     }
 }
