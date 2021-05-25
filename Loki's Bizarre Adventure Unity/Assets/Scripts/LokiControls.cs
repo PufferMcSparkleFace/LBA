85594b13aae43f9908d9929295e3a6a9fcbb665b
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2021-05-26 01:50:00 +1200

    trying without lastmove

--- Loki's Bizarre Adventure Unity/Assets/Scripts/LokiControls.cs
+++ Loki's Bizarre Adventure Unity/Assets/Scripts/LokiControls.cs
@@ -244,14 +244,14 @@ public class LokiControls : MonoBehaviour
             canclonebounce = false;
         }
 
-        lastMove = new Vector2(move.x, rb.velocity.y);
+      
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Mirror" && canbounce == true)
         {
-            var direction = Vector3.Reflect(lastMove.normalized, collision.contacts[0].normal);
+            var direction = Vector3.Reflect(rb.velocity.normalized, collision.contacts[0].normal);
             rb.velocity = direction * Mathf.Max(rb.velocity.x, 0f) * mirrorboostamount;
             canbouncetimer = startingcanbouncetimer;
             Debug.Log("We Hit the Mirror");
