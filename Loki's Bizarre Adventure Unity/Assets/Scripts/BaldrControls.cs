9b8b68255debff44a0b0e7160a7d412e389c9f77
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2021-05-28 14:38:48 +1200

    i dont understand why this isn't working

--- Loki's Bizarre Adventure Unity/Assets/Scripts/BaldrControls.cs
+++ Loki's Bizarre Adventure Unity/Assets/Scripts/BaldrControls.cs
@@ -234,12 +234,13 @@ public class BaldrControls : MonoBehaviour
                 
             }
         }
+
         if(BaldrSpriteRenderer.flipX == true)
         {
             shieldgameobject.transform.eulerAngles = new Vector3(shieldgameobject.transform.eulerAngles.x, 180, shieldgameobject.transform.eulerAngles.z);
             shieldgameobject.transform.position = position1.transform.position;
         }
-        else
+        if (BaldrSpriteRenderer.flipX == false)
         {
             shieldgameobject.transform.eulerAngles = new Vector3(shieldgameobject.transform.eulerAngles.x, 0, shieldgameobject.transform.eulerAngles.z);
             shieldgameobject.transform.position = position2.transform.position;
