4bfdfd5df834cfa7a4af5fa893ca54b465ab44d1
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2021-05-30 15:11:11 +1200

    Importing Assets

---
+++ Loki's Bizarre Adventure Unity/Assets/Scripts/DoorSwitch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSwitch : MonoBehaviour
+{
+    public bool sticky = true;
+    public GameObject wall;
+    public SpriteRenderer switchunpressed;
+    public SpriteRenderer switchpressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switchpressed.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Clone" || collision.gameObject.tag == "Loki" || collision.gameObject.tag == "Baldr")
+        {
+            switchpressed.enabled = true;
+            switchunpressed.enabled = false;
+            wall.SetActive(false);
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(sticky == false)
+        {
+            if (collision.gameObject.tag == "Clone" || collision.gameObject.tag == "Loki" || collision.gameObject.tag == "Baldr")
+            {
+                switchpressed.enabled = false;
+                switchunpressed.enabled = true;
+                wall.SetActive(true);
+            }
+        }
+        
+    }
+}
