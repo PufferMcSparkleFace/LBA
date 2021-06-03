def956fe68add36afcc05bd4b296a1609427caf1
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2021-06-03 17:32:54 +1200

    im monkey mode

--- Loki's Bizarre Adventure Unity/Assets/Scripts/DoorSwitch.cs
+++ Loki's Bizarre Adventure Unity/Assets/Scripts/DoorSwitch.cs
@@ -20,12 +20,16 @@ public class DoorSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(clonescript.ispressingswitch == false && lokiscript.ispressingswitch == false && baldrscript.ispressingswitch == false)
+        if(sticky == false)
         {
-            switchpressed.enabled = false;
-            switchunpressed.enabled = true;
-            wall.SetActive(true);
+            if (clonescript.ispressingswitch == false && lokiscript.ispressingswitch == false && baldrscript.ispressingswitch == false)
+            {
+                switchpressed.enabled = false;
+                switchunpressed.enabled = true;
+                wall.SetActive(true);
+            }
         }
+    
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
